6ba837024545d7895c9829db056e94fc846832c9
Basic <basic@localhost>
2015-11-30 06:18:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# apache-openaz
+
+## Quick Instructions for Jetty 8
+
+1.  Copy xacml directory to /opt/xacml
+
+    $ cp -r xacml /opt/xacml
+
+2.  Create three empty installations of Jetty 8:
+
+    * /opt/jetty8-pap-admin
+    * /opt/jetty8-pap-rest
+    * /opt/jetty8-pdp-rest
+
+3.  Configure Jetty ports
+
+    *   /opt/jetty8-pap-admin/etc/jetty.xml
+
+        <Set name="port"><Property name="jetty.port" default="9999"/></Set>
+        <Set name="confidentialPort">9998</Set>
+
+    *   /opt/jetty8-pap-rest/etc/jetty.xml
+
+        <Set name="port"><Property name="jetty.port" default="9090"/></Set>
+        <Set name="confidentialPort">9091</Set>
+
+    *   /opt/jetty8-pdp-rest/etc/jetty.xml
+
+        <Set name="port"><Property name="jetty.port" default="8080"/></Set>
+        <Set name="confidentialPort">8081</Set>
+
+4.  Install Maven
+
+5.  From the base source directory (containing this file), run the build command:
+
+    $ mvn package
+
+6.  Copy war files into Jetty webapp directories
+
+    $ cp --force openaz-xacml-pap-admin/target/openaz-xacml-pap-admin-0.0.1-SNAPSHOT.war /opt/jetty8-pap-admin/webapps/admin.war
+    $ cp --force openaz-xacml-pap-rest/target/openaz-xacml-pap-rest-0.0.1-SNAPSHOT.war /opt/jetty8-pap-rest/webapps/pap.war
+    $ cp --force openaz-xacml-pdp-rest/target/openaz-xacml-pdp-rest-0.0.1-SNAPSHOT.war /opt/jetty8-pdp-rest/webapps/pdp.war
+
+7.  Run each Jetty server
+
---
+++ openaz-xacml/src/main/java/org/apache/openaz/xacml/std/pip/engines/ConfigurableResolver.java
@@ -0,0 +1,55 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.apache.openaz.xacml.std.pip.engines;
+
+import java.util.Collection;
+import java.util.Properties;
+
+import org.apache.openaz.xacml.api.pip.PIPException;
+import org.apache.openaz.xacml.api.pip.PIPRequest;
+
+public interface ConfigurableResolver {
+    /**
+     * Configures this <code>JDBCResolver</code> using the given <code>Properties</code>>
+     *
+     * @param id the <code>String</code> identifier for locating properties for this <code>JDBCResolver</code>
+     * @param properties the <code>Properties</code> to search for properties
+     * @param defaultIssuer the default issuer value if none is defined specifically.
+     * @throws org.apache.openaz.xacml.api.pip.PIPException if there is an error configuring this
+     *             <code>JDBCResolver</code>
+     */
+    void configure(String id, Properties properties, String defaultIssuer) throws PIPException;
+
+    /**
+     * Adds attributes required by the resolver to return an attribute.
+     *
+     * @param attributes - A modifiable collection
+     */
+    void attributesRequired(Collection<PIPRequest> attributes);
+
+    /**
+     * Adds attributes provided by the resolver.
+     *
+     * @param attributes - A modifiable collection
+     */
+    void attributesProvided(Collection<PIPRequest> attributes);
+
+}
